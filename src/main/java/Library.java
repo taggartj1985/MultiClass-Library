c4635540bfe48ed6d0b1e71d2e11af8555c8b793
John Taggart <taggartj1985@gmail.com>
2020-05-26 16:27:44 +0100

    mvp done

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private int capacity;
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.capacity = 5;
+        this.books = new ArrayList<Book>();
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int bookCount(){
+        return this.books.size();
+    }
+
+    public void addBook(Book book){
+        int currentCapacity = bookCount();
+        if (currentCapacity < this.capacity){
+            this.books.add(book);
+        }
+    }
+
+    public void removeBook(Book book){
+        this.books.remove(this.books.indexOf(book));
+    }
 }
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,61 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    Library library;
+    Book book1;
+    Book book2;
+    Book book3;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book("It", "Stephen King", "Horror");
+        book2 = new Book("Dracula", "Bram Stoker", "Horror");
+        book3 = new Book("The Dark Knight Returns", "Frank Miller", "Action");
+    }
+
+    @Test
+    public void addBooksInLibrary(){
+        library.addBook(book1);
+        assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void checkCapacity(){
+        assertEquals(5,library.getCapacity());
+    }
+
+    @Test
+    public void addBookIfNotFull(){
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        assertEquals(5, library.bookCount());
+    }
+
+    @Test
+    public void cantAddBookIfFull(){
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        assertEquals(5, library.bookCount());
+    }
+
+    @Test
+    public void removeBook(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.removeBook(book3);
+        assertEquals(2, library.bookCount());
+    }
+}
